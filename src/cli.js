566449035b2e4d7b8a5da2b5eaaee67e9ce0e505
Kavanagh, Luke <ltkavanagh@gmail.com>
2020-02-27 00:22:08 +0000

    Begin CLI

---
+++ src/cli.js
@@ -0,0 +1,39 @@
+const program = require('commander');
+const { key } = require('../config'); // todo dont do this
+const SheetReader = require('./index');
+
+program
+	.option('-s, --spreadsheet <id>', 'Spreadsheet ID')
+	.option('-h, --sheets <list>', 'Sheet names, comma delimited list')
+	.option('-e, --auth-email', 'Google API service account email')
+	.option('-x, --auth-subject', 'Google API subject user email')
+	.option('-k, --auth-key', 'Google API key')
+
+program.parse(process.argv)
+
+const SUBJECT = process.env.GOOGLE_USER;
+const EMAIL = process.env.GOOGLE_CLIENT_EMAIL;
+const KEY = process.env.GOOGLE_SERVICE_KEY || process.env.GOOGLE_KEY || key;
+
+(async () => {
+
+	try {
+		const options = {
+			columnKeys: 'legacy',
+		};
+	
+		const auth = {
+			key: KEY,
+			email: EMAIL,
+			subject: SUBJECT,
+		};
+
+		const spreadsheetId = program.spreadsheet;
+		const sheetNames = program.sheets.split(/,/g).map(s => s.trim()).filter(Boolean);
+		const data = await SheetReader({ auth }).fetchSheet(spreadsheetId, sheetNames, options);
+	
+		console.log(data);
+	} catch (error) {
+		console.error(error);
+	}
+})();
