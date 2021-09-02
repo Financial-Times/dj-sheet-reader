b6c774ef591705be7c9b842cc509ef6aa4987ef9
kavanagh <kavanagh@users.noreply.github.com>
2021-09-02 18:41:32 +0100

    Add and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	env: {
+		node: true,
+		jest: true,
+		es6: true,
+	},
+	extends: [
+		'plugin:jest/recommended',
+		'plugin:jest-formatting/recommended',
+		'plugin:prettier/recommended',
+		'prettier',
+		'eslint:recommended',
+	],
+	plugins: ['jest-formatting'],
+}
